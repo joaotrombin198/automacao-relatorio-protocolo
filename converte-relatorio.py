49ef2b29f9908b2a784e1189bd5332a2739b5d08
joaotrombin198 <joao.trombin@criciuma.unimedsc.com.br>
2025-07-21 15:48:09 -0300

    Update converte-relatorio.py

--- converte-relatorio.py
+++ converte-relatorio.py
@@ -225,7 +225,7 @@ if arquivo_novo.lower().endswith('.xls'):
 else:
     caminho_para_uso = arquivo_novo
 
-data_ontem = (datetime.now() - timedelta(days=3)).strftime('%Y-%m-%d')
+data_ontem = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
 
 # Colunas que são datas no relatório (1-based)
 colunas_datas = [3, 7]
