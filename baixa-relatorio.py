2c944d43536fcd1111bf22a2677ee23e43885175
joaotrombin198 <joao.trombin@criciuma.unimedsc.com.br>
2025-07-22 16:17:44 -0300

    Update baixa-relatorio.py

--- baixa-relatorio.py
+++ baixa-relatorio.py
@@ -29,7 +29,7 @@ while True:
         }
 
         options = webdriver.ChromeOptions()
-        #options.add_argument("--headless=new")
+        options.add_argument("--headless=new")
         options.add_argument("--disable-gpu")
         options.add_argument("--window-size=1920,1080")
         options.add_argument("--no-sandbox")
